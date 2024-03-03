1970b24ab1ff3e01a3eb6478e8420f9bba85e571
darvy <darvybetance@gmail.com>
2024-03-02 20:55:43 -0400

    Intentando resolver errores.

--- practica-5/SensorDataJMS/src/main/java/com/pucmm/eict/sensordatajms/controller/SensorController.java
+++ practica-5/SensorDataJMS/src/main/java/com/pucmm/eict/sensordatajms/controller/SensorController.java
@@ -3,16 +3,18 @@ package com.pucmm.eict.sensordatajms.controller;
 import com.pucmm.eict.sensordatajms.model.SensorData;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Controller
 public class SensorController {
     private final Map<UUID, SensorData> sensorDataMap = new HashMap<>();
 
-    @MessageMapping("/sensorData")
-    @SendTo("/topic/sensorData")
+    @GetMapping("/hola")
     public Map<UUID, SensorData> handleSensorData(SensorData sensorData) {
         sensorDataMap.put(sensorData.getIdDispositivo(), sensorData);
         System.out.println("Data Recibida: " + sensorData);
