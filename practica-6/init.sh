df93a45a117bb80db0b8a859dda1d433478e830c
axba0001 <axba0001@ce.pucmm.edu.do>
2024-03-17 16:45:53 -0400

    error de dominio xd

--- practica-6/init.sh
+++ practica-6/init.sh
@@ -24,10 +24,10 @@ sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install certbot
 
-sudo certbot certonly --standalone -d prac-6.turnos.do/ -v
+sudo certbot certonly --standalone -d p6.turnos.do/ -v
 
 sudo mkdir -p /etc/haproxy/certs
-DOMAIN='prac-6.turnos.do'
+DOMAIN='p6.turnos.do'
 sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem >` /etc/haproxy/certs/$DOMAIN.pem'
 sudo chmod -R go-rwx /etc/haproxy/certs
 
