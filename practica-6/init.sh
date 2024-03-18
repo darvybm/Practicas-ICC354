d643484db1ca6cc5c2617e911da2c0ed89d867a5
axba0001 <axba0001@ce.pucmm.edu.do>
2024-03-18 01:06:45 -0400

    prueba

--- practica-6/init.sh
+++ practica-6/init.sh
@@ -3,7 +3,7 @@
 echo "Configuración básica de HAProxy"
 
 # Eliminar todas las aplicaciones Java.
-killall java
+pkill java
 
 # Crear una copia del archivo de configuración actual.
 if [ ! -e "/etc/haproxy/haproxy.cfg.original" ]; then
@@ -12,19 +12,19 @@ if [ ! -e "/etc/haproxy/haproxy.cfg.original" ]; then
 fi
 
 # Copiar el archivo de configuración de HAProxy.
-sudo cp Practicas-ICC354/practica-6/haproxy.cfg /etc/haproxy/haproxy.cfg
+sudo cp haproxy.cfg /etc/haproxy/haproxy.cfg
 
-# Instalando docker
-sudo apt install docker-compose
+# Instalando docker y docker-compose
+sudo apt update
+sudo apt install -y docker.io docker-compose
 
-# Instalando certificados SSL
+# Instalando certbot y generando certificados SSL
 sudo apt-get remove certbot
 sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install certbot
-
+sudo apt-get install -y certbot
 
 # Dominio para el que se generará el certificado SSL
 DOMAIN="pract6.turnos.do"
@@ -36,7 +36,6 @@ LE_DIR="/etc/letsencrypt/live/$DOMAIN"
 CERT_DIR="/etc/ssl/certs"
 
 # Comando para obtener el certificado SSL utilizando certbot de Let's Encrypt
-#sudo certbot certonly --standalone -d "$DOMAIN" -v
 sudo certbot certonly --standalone --preferred-challenges http -d $DOMAIN
 
 sudo mkdir -p "$CERT_DIR"
@@ -45,7 +44,6 @@ cat "$LE_DIR/fullchain.pem" "$LE_DIR/privkey.pem" > "$CERT_DIR/$DOMAIN.pem"
 sudo chmod -R go-rwx "$CERT_DIR"
 
 # Reiniciar el contenedor de HAProxy para que tome los nuevos certificados
-sudo service haproxy restart
+sudo systemctl restart haproxy
 
 echo "Certificado SSL generado y configurado correctamente."
-
